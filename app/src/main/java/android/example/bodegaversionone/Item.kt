e54fb55752af1030bc82f7d8f562af65b93f307d
Derek <derekwellis@outlook.com>
2022-03-16 13:12:31 -0500

    Adds Item class for table enquiry Adds DAO entity for Room Adds Room repository Adds Room DB Adds Item ViewModel and MVVM architecture Errors in ItemViewModel, related to dependencies

---
+++ app/src/main/java/android/example/bodegaversionone/Item.kt
@@ -0,0 +1,16 @@
+package android.example.bodegaversionone
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+data class Item(val data: MyDataItem) {
+
+    // creating an SQL table for Room
+    @Entity(tableName = "item_table")
+    // adding keys to look up items
+    private class Item(
+                        @PrimaryKey(autoGenerate = true) val title: String,
+                        @ColumnInfo(name = "category") val category: String
+                        )
+}
---
+++ app/src/main/java/android/example/bodegaversionone/ItemViewModel.kt
@@ -0,0 +1,30 @@
+package android.example.bodegaversionone
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+
+
+class ItemViewModel(private val repository: ItemRepository) : ViewModel() {
+
+    // uses LiveData and caching on allItems
+    // puts the observer on the data to only update when changed
+    // Repository is now separated from UI through the ViewModel MVVM
+    val allItems : LiveData<List<MyDataItem>> = repository.allItems.asLiveData()
+
+    // launches coroutine to insert the data without blocking
+    fun insert(item: MyDataItem) =  viewModelScope {
+        repository.insert(item)
+    }
+}
+
+class ItemViewModelFactory(private val repository: ItemRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>) : T {
+        if(modelClass.isAssignableFrom(ItemViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return ItemViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel Class")
+    }
+}
